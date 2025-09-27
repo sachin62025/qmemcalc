52d1fc8ed0f5f9b4ed69d4ca4807a6670b7a5351
sachin62025 <sachinkumar18449@iiitmanipur.ac.in>
2025-09-27 15:11:18 +0530

    Prepare package for PyPI

--- setup.py
+++ setup.py
@@ -8,14 +8,25 @@ setup(
     description="GPU Memory Estimator for QLoRA / LoRA / Transformers",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
+    url="https://github.com/sachin62025/qmemcalc",
+    license="MIT",
     packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
         "torch>=2.0",
         "transformers>=4.40",
     ],
+    extras_require={
+        "dev": ["pytest"]
+    },
+    entry_points={
+        "console_scripts": [
+            "qmemcalc = qmemcalc.memory:main"
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
     ],
 )
