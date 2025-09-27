c00b64ddbfb45c871855bd48c5fc39f215b955a0
sachin62025 <sachinkumar18449@iiitmanipur.ac.in>
2025-09-27 15:18:22 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/sachin62025/qmemcalc",
-    license="MIT",
+    license="Apache-2.0",
     packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
